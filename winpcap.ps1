c6d6f90215a874035fbdd49a84809ada9af87790
cyberheisen <62576899+cyberheisen@users.noreply.github.com>
2022-05-11 09:37:17 -0500

    Update winpcap.ps1

--- winpcap.ps1
+++ winpcap.ps1
@@ -1,7 +1,8 @@
 $etl_file = ".\NetTrace.etl"
+$pcap_file = ".\NetTrace.pcap"
 $trace_start = "netsh trace start capture=yes tracefile=" + $etl_file
 $trace_stop = "netsh trace stop"
-$etl2pcap = ".\etl2pcapng " + $etl_file
+$etl2pcap = ".\etl2pcapng " + $etl_file + " " + $pcap_file
 $start_results = Invoke-Expression -Command $trace_start
 
 $start_results
@@ -9,10 +10,9 @@ Write-Host "Press any key to end the packet trace"
 $null = $Host.UI.RawUI.Readkey('NoEcho, IncludeKeyDown')
 Write-Host "Stopping trace"
 
-$end_results = Invoke-Expression -Command $trace_stop
+$end_results = Invoke-Expression -command $trace_stop
 $end_results
 Write-Host "Converting ETL file to pcap format"
 
-$etl_convert = Invoke-Expression -Command $etl2pcap
+$etl_convert = Invoke-Expression -command $etl2pcap
 Write-Host $etl_convert
-
