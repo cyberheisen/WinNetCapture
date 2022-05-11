1e9bbe51d458d9ea865667f113238a9c516ae469
cyberheisen <62576899+cyberheisen@users.noreply.github.com>
2022-05-11 09:27:56 -0500

    Update winpcap.ps1

--- winpcap.ps1
+++ winpcap.ps1
@@ -9,10 +9,10 @@ Write-Host "Press any key to end the packet trace"
 $null = $Host.UI.RawUI.Readkey('NoEcho, IncludeKeyDown')
 Write-Host "Stopping trace"
 
-$end_results = Invoke-Expression -command $trace_stop
+$end_results = Invoke-Expression -Command $trace_stop
 $end_results
 Write-Host "Converting ETL file to pcap format"
 
-$etl_convert = Invoke-Expression -command $etl2pcap
+$etl_convert = Invoke-Expression -Command $etl2pcap
 Write-Host $etl_convert
 
